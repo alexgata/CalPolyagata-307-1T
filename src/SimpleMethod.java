9ea5f24f1b3cbb58bc945b7b02dcf3e77ddab670
alexgata <agata@calpoly.edu>
2018-10-05 10:45:34 -0700

    yes

---
+++ src/SimpleMethod.java
@@ -0,0 +1,11 @@
+
+public class SimpleMethod {
+	public static void main(String[] args) {
+		mult(2,2);
+	}
+	public static int mult(int x, int y){
+		int result = x*y;
+		System.out.println(result);
+		return result;
+	}
+}
